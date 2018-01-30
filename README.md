72e76d6c96c3e89bc386366928a7284f4bacd9f3
Matheus Compri <matheus.compri@gmail.com>
2018-01-30 21:19:06 -0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Markbin
+
+Markbin is a collaborative markdown editor, built with Meteor and React. It was created in the [Introduction to Meteor Development](https://www.coursera.org/learn/meteor-development) course classes.
+
+# Instructions
+
+To run this project use `meteor run` command.
---
+++ client/components/bins/bins_viewer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { markdown } from 'markdown';
+
+class BinsViewer extends Component {
+  render(){
+    const rawHTML = markdown.toHTML(this.props.bin.content);
+    return (
+      <div className="col-xs-4">
+        <h5>output</h5>
+        <div dangerouslySetInnerHTML={{ __html: rawHTML }}></div>
+      </div>
+    );
+  }
+}
+
+export default BinsViewer;
